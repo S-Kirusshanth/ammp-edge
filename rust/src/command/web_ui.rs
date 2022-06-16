c1bd5e63d2b93aea2919879f0514390633e630be
Svet Bajlekov <tomatoman@gmail.com>
2022-06-16 07:46:58 -0700

    listen on all local interfaces

--- rust/src/command/web_ui.rs
+++ rust/src/command/web_ui.rs
@@ -1,12 +1,10 @@
-use anyhow::Result;
-
 use crate::web_ui::router;
 
-pub fn web_ui() -> Result<()> {
-    println!("Now listening on localhost:8000");
+pub fn web_ui() {
+    println!("Now listening on 0.0.0.0:8000");
 
     // The `start_server` starts listening forever on the given address.
-    rouille::start_server("localhost:8000", move |request| {
+    rouille::start_server("0.0.0.0:8000", move |request| {
         router(request)
     });
 }
